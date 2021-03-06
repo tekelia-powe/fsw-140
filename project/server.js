736962604c2ddd0275ac6e27e200449c0f929175
tekelia-powe <66314860+tekelia-powe@users.noreply.github.com>
2021-03-06 08:50:51 -0600

    Week 6 Project

--- project/server.js
+++ project/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan')
-const bodyParser = require('body-parser')
+
 
 app.use(express.json());
 app.use(morgan('dev'))
